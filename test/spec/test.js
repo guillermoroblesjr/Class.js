10522022d7ea0ec4f858b5d2a14d7c286cd0be50
Guillermo Robles, Jr <will.robles@gmail.com>
2015-02-16 00:47:42 -0600

    adding tests

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,19 +3,58 @@
 (function () {
   'use strict';
   
-  describe("Class", function() {
-    describe("constructor", function() {
-      it("should have a default name", function() {
-        //var classInstance = new Class();
-        expect(Class.temp()).to.equal("I am a temp function.");
+  describe("Class -- ", function() {
+
+    var SomeClass;
+    var classInstance;
+    
+    describe('creating a class', function(){
+
+      it('should return a function', function(){
+        SomeClass = Class.make(function SomeClass(){
+        });
+        expect(SomeClass).to.be.a('function');
       });
 
-      // it("should set cow's name if provided", function() {
-      //   var cow = new Cow("Kate");
-      //   expect(cow.name).to.equal("Kate");
-      // });
+      it('should create a class instance', function(){
+        classInstance = new SomeClass();
+        expect(classInstance).to.be.instanceof(SomeClass);
+      });
+
+      it('should return all the properties', function(){
+        // var SomeClass = Class.make(function SomeClass(){
+        //   this.property1 = 1;
+        //   // this.animalFunctionToBeInherited = function(){
+        //   //   console.log('I am added to the prototype!');
+        //   // };
+        //   return this;
+        // });
+        // var classInstance = new SomeClass();
+        // var properties = classInstance.getAllProperties();
+        // expect(properties).to.have.property('property1');
+      });
+
+      it('should turn the class properties to prototypes', function(){
+        // var SomeClass = Class.make(function SomeClass(){
+        //   this.animalThing = 2;
+        //   // this.animalFunctionToBeInherited = function(){
+        //   //   console.log('I am added to the prototype!');
+        //   // };
+        //   return this;
+        // });
+        // expect()
+      });
     });
 
+    it('should turn the class methods to prototypes', function(){
+      SomeClass = Class.make(function SomeClass(){
+        this.animalThing = 2;
+        // this.animalFunctionToBeInherited = function(){
+        //   console.log('I am added to the prototype!');
+        // };
+        return this;
+      });
+    });
     // describe("#greets", function() {
     //   it("should throw if no target is passed in", function() {
     //     expect(function() {
